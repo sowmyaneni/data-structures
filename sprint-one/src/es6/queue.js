4dc0a57828bd61dbb4189198197818a7a4786853
Sowmya Surapaneni <ssneni@gmail.com>
2019-04-10 21:02:21 -0700

    Complete pseudoclassical and es6 instantiation patterns

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.key] = value;
+    this.key++;
+  }
+
+  dequeue() {
+    var tempKey = Object.keys(this.storage)[0];
+    var tempStorage = this.storage[tempKey];
+    delete this.storage[tempKey];
+    return tempStorage;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
+  }
+  
+  push(value) {
+    this.storage[this.key] = value;
+    this.key++;
+  }
+
+  pop() {
+    if (this.key >= 0) {
+      delete this.storage[this.key];
+    }
+    if (this.key > 0) {
+      this.key--;
+    }
+    return this.storage[this.key];
+  }
+
+  size() {
+    return this.key;
   }
 
 }
