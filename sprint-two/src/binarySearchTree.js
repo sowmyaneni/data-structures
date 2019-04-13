7de97006da79f12e3ad87c2a7e4050818e0cf6dc
sowmyaneni <ssneni@gmail.com>
2019-04-13 13:11:36 -0700

    adds hashtable and progress for bnTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,62 @@
 var BinarySearchTree = function(value) {
+    // var obj = {}
+    // var root = new Branch(value)
+    // obj = Object.create(BinarySearchTreeMethods);
+    // obj.root = root;
+    // return obj
 };
 
+BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(value) {
+    var branch = new Branch(value)
+    
+    // if (this.root === null) {
+    //     this.root = branch;
+    //     return;
+    // }
+
+    var currentbranch = this.root;
+    
+    while (currentbranch) {
+        if(currentbranch.value > branch.value ) {
+            if (!currentbranch.left) {
+                currentbranch.left = branch.value;
+            } else{
+                currentbranch = currentbranch.left;       
+            }   
+        } else {
+            if (!currentbranch.right) {
+                currentbranch.right = branch.value;
+            } else{
+                currentbranch = currentbranch.right;       
+            } 
+        }
+    }
+
+};
+
+BinarySearchTreeMethods.contains = function(value, branch = this.root) {
+    if (branch.value === value) { 
+        return true; 
+    }
+
+    if (branch.value > value) {
+        return this.contains(value, branch.left);
+    } else {
+        return this.contains(value, branch.right);
+    }
+};
+
+BinarySearchTreeMethods.depthFirstLog = function(cb) {
+
+};
+
+var Branch = function(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,56 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = [k,v];
+  //check to see if anything is at index
+  if(this._storage[index] === undefined) {
+    //store key value bucket at this index
+    this._storage[index] = [bucket];
+    //this._storage[index] = [bucket]
+    return;
+  } else {
+      for (var i = 0; i < this._storage[index].length; i++) {
+        if(this._storage[index][i][0] === k){
+          this._storage[index][i][1] = v;
+          return;
+        }
+      }
+  //push the new key value pair
+    this._storage[index].push(bucket);
+  }  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if(this._storage[index][i][0] === k){
+        return this._storage[index][i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if(this._storage[index][i][0] === k){
+        return this._storage[index][i].splice(this._storage[index][i],1);
+      }
+    }
+  }
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  //insert: average case: O(1), worst case O(n)
+  //retrieve: average case: O(1), worst case O(n)
+  //remove: average case: O(1), worst case O(n)
  */
 
 
