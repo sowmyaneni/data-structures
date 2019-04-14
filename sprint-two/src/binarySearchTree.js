a32d2a2f10265cfe928a0e1f570fb340eab84b4f
sowmyaneni <ssneni@gmail.com>
2019-04-13 17:12:56 -0700

    Update binary search tree - complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,63 +1,94 @@
 var BinarySearchTree = function(value) {
-    // var obj = {}
-    // var root = new Branch(value)
-    // obj = Object.create(BinarySearchTreeMethods);
-    // obj.root = root;
-    // return obj
+    var obj = {}
+    obj = Object.create(BinarySearchTreeMethods);
+    obj.value = value;
+    obj.left = null;
+    obj.right = null;
+    return obj;
 };
 
-BinarySearchTreeMethods = {};
+// var Branch = function(value){
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+// }
+
+BinarySearchTreeMethods = {}
 
 BinarySearchTreeMethods.insert = function(value) {
-    var branch = new Branch(value)
-    
-    // if (this.root === null) {
-    //     this.root = branch;
-    //     return;
-    // }
+    var branch = BinarySearchTree(value)
 
-    var currentbranch = this.root;
-    
-    while (currentbranch) {
-        if(currentbranch.value > branch.value ) {
-            if (!currentbranch.left) {
-                currentbranch.left = branch.value;
-            } else{
-                currentbranch = currentbranch.left;       
-            }   
+    if(this.value > branch.value){
+        if(!this.left){
+            this.left = branch
+            return;
         } else {
-            if (!currentbranch.right) {
-                currentbranch.right = branch.value;
-            } else{
-                currentbranch = currentbranch.right;       
-            } 
+            this.left.insert(value)
+        }
+
+    } else {
+        if(!this.right){
+            this.right = branch
+            return;
+        } else {
+            this.right.insert(value)
         }
     }
+    // Insertion using while loop below
+    // while (currentbranch) {
+    //     if(currentbranch.value > branch.value ) {
+    //         if (!currentbranch.left) {
+    //             currentbranch.left = branch;
+    //             return;
+    //         } else{
+    //             currentbranch = currentbranch.left;       
+    //         }   
+    //     } else {
+    //         if (!currentbranch.right) {
+    //             currentbranch.right = branch;
+    //             return;
+    //         } else{
+    //             currentbranch = currentbranch.right;       
+    //         } 
+    //     }
+    // }
 
 };
 
-BinarySearchTreeMethods.contains = function(value, branch = this.root) {
-    if (branch.value === value) { 
+
+
+BinarySearchTreeMethods.contains = function(value) {
+    if (this.value === value) { 
         return true; 
     }
 
-    if (branch.value > value) {
-        return this.contains(value, branch.left);
-    } else {
-        return this.contains(value, branch.right);
+    if (this.left && this.value > value) {
+        return this.left.contains(value);
+    } else if (this.right) {
+        return this.right.contains(value);
     }
+
+    return false;
 };
 
-BinarySearchTreeMethods.depthFirstLog = function(cb) {
+BinarySearchTreeMethods.depthFirstLog = function(cb)  {
+    // if left and right both equal null
+    cb(this.value);
+
+    if(this.left !== null) { 
+        this.left.depthFirstLog(cb);
+    }
+    if(this.right !== null){
+        this.right.depthFirstLog(cb);
+    }
 
 };
 
-var Branch = function(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-}
+var newTree = BinarySearchTree(3);
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ //insert O(log n)
+ //contains O(log n)
+ //depthFirstLog O(log n)
  */
